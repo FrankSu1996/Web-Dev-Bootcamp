b35ca426a239fe1c4c6f9573f0b2b2a3847b1a6f
Frank <franksu49@gmail.com>
2019-12-08 19:59:06 -0500

    started javascsript unit, finished bootstrap4

---
+++ Javascript/stalker.js
@@ -0,0 +1,6 @@
+var firstName = prompt("What is your first name");
+var lastName = prompt("What is your last name");
+var age = prompt("What is your age?");
+
+console.log("Hi " + firstName + lastName + " nice to meet you!");
+console.log("You are " + age + "years old!");
