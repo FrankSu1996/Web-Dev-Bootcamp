6e54891db24393b3674052b12e67bb9d79382fc4
Frank <franksu49@gmail.com>
2019-12-14 23:35:23 -0500

    finished API lesson

---
+++ API/first_request.js
@@ -0,0 +1,10 @@
+var request = require('request');
+request('http://www.reddit.com', function(error, response, body) {
+    if (error) {
+        console.log("Something went wrong!!");
+        console.log(error);
+    }
+    else if(response.statusCode == 200) {
+        console.log(body);
+    }
+});
